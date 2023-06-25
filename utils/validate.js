b7a52d80688e24c0956cdc4b107f219520c96079
InEmEcI <iikar@bk.ru>
2023-06-25 21:57:48 +0300

    14 на авто-тесты

--- utils/validate.js
+++ utils/validate.js
@@ -21,19 +21,6 @@ const createUserVal = celebrate({
   }),
 });
 
-// const createUserVal = celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().min(2).max(30),
-//     abour: Joi.string().min(2).max(30),
-//     avatar: Joi.string().regex(
-//       // eslint-disable-next-line comma-dangle
-//       /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/
-//     ),
-//     email: Joi.string().email().required(),
-//     password: Joi.string().required(),
-//   }),
-// });
-
 // const getUsersVal = {
 //     text: Joi.string().required().min(2),
 // }
